7dd6f0e6f2918a535a94cb64a7edc7fcdbd8d91f
uridiagiorgi <uridiagiorgi01@gmail.com>
2024-06-26 13:29:40 +0400

    second commit

---
+++ Day 01/index.py
@@ -0,0 +1,27 @@
+name = "giorgi"
+#name aris cvladi
+#= aris cvladis mnishvnelobis mimnichebeli simbolo
+#'nika' aris cvladistvis mnishvneloba
+surname = "uridia"
+print(name)
+#print funqcias gadaecema ekranze gamosatani cvladi an stringi
+name = "giorgi" #aris str tipis cvladi
+age = 12 #aris integer mteli ricxvi
+height = 164.5 #es aris float tipis cvladi (atwiladi)
+knows_programming = True #boolean tipis cvladi
+is_ugly = False #boolean tipis cvladi
+print(name + surname)
+print(type(age)) #gveubneba age-s tips
+print(type(name)) #gveubneba name-s tips
+print(type(surname)) #gveubneba surname-s tips
+print(type(height)) #gveubneba height-s tips
+print(type(knows_programming)) #gveubneba knows_programming-s tips
+print(name + str(age))
+name_1 = "chemi"
+name_2 = "saxelia"
+name_3 = "me"
+name_4 = "var"
+name_5 = "wlis"
+name_6 = "simaglea"
+name_7 = "santimetri"
+print(name_1 + " " + name_2 + " " + name + " " + name_3 + " " + name_4 + " " + str(age) + " " + name_5 + " " + name_1 + " " + name_6 + " " + str(height) + " " + name_7)
