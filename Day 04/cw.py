0debb4b4571728f4d854f8c3d6ae3538cd5e969a
uridiagiorgi <qweqwe@gmail.com>
2024-07-10 15:39:36 +0400

    second commit

---
+++ Day 04/cw.py
@@ -0,0 +1,13 @@
+#name unda iyos ertidaimave raodenobis brwyalebisgan
+name = "surname"
+print("name')
+#gaertes mere unda eweros =
+gaerte->have fun
+#remote_control aris swori porma
+remote control = 15
+#names shwirdeba ertnairi frchxili
+print{name)
+#names gverdze komentaris nishani ar undas
+#name = "secret"
+print (secret)
+
---
+++ Day 04/hw.py
@@ -0,0 +1,19 @@
+print(50 + 20)
+print(90-27)
+print(67*12)
+print(40/8)
+last_name = "uridia"
+print(last_name)
+#name unda iyos ertidaimave raodenobis brwyalebisgan
+name = "surname"
+print("name')
+#gaertes mere unda eweros =
+gaerte->have fun
+#remote_control aris swori porma
+remote control = 15
+#names shwirdeba ertnairi frchxili
+print{name)
+#names gverdze komentaris nishani ar undas
+#name = "secret"
+print (secret)
+
