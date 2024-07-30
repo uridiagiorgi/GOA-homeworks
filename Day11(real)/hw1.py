a9723dd9d24e8a7bc4e1cdbe36f4f71e07da439a
uridiagiorgi <qweqwe@gmail.com>
2024-07-30 15:58:40 +0400

    second commit

---
+++ Day11(real)/hw1.py
@@ -0,0 +1,39 @@
+a = True
+b = False
+print(a and b)
+
+k = False
+t = True
+print(k and t)
+
+s = True
+o = True
+print(s and o)
+
+l = False
+l_1 = False
+print(l and l_1)
+
+g = True
+j = False
+print(g and j)
+
+r = True
+i = False
+print(r or i)
+
+p = False
+d = False
+print(p or d)
+
+y = False
+e = True
+print(y or e)
+
+w = True
+z = True
+print(w or z)
+
+c = False
+m = True
+print(c or m)
---
+++ Day11(real)/hw3.py
@@ -0,0 +1,41 @@
+num = 28
+print(num<27)
+
+age = 13
+Decision = (age >=12)
+print(Decision)
+
+Euro = 2024
+world_cup = 2026
+print(Euro<=world_cup)
+ 
+year = 2023
+next_year = 2024
+print(next_year!=year)
+
+Messi = 10
+Ronaldo = 7
+print(Messi>=Ronaldo)
+
+Height = 160.50
+Height_in_a_year = 165
+print(Height_in_a_year==Height)
+
+diamond = 24
+coal = 100
+print(diamond<=coal)
+
+Notifications = 99
+Notifications_in_10_minutes = 109
+print(Notifications_in_10_minutes!=Notifications)
+
+sugar = 1000
+diabetes = sugar>999
+print(diabetes)
+
+phone_battery = 96
+phone_battery_in_an_hour = 19
+print(phone_battery_in_an_hour==phone_battery)
+
+
+
