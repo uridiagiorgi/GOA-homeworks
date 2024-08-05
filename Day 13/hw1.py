ad532edc889856ea1fee7cc0a825d6809ae5c21f
uridiagiorgi <qweqwe@gmail.com>
2024-08-05 11:09:53 +0400

    second commit

---
+++ Day 13/hw1.py
@@ -0,0 +1,3 @@
+#sequencing aris is rom kodi miyveba zemodan qvemot mimsevrobit
+#iteration aris instruqciis ganmeorebit shesruleba
+#selection gansazgvravs rodis unda gayves titoeul gzas
---
+++ Day 13/hw2.py
@@ -0,0 +1,3 @@
+age = int(input())
+father_age = int(input())
+print((father_age + 23) / (age + 23))
