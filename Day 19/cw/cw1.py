0e7a8604e6060e12e9a5709939fecaddf1e2bb20
uridiagiorgi <qweqwe@gmail.com>
2024-08-23 17:25:26 +0400

    second commit

---
+++ Day 19/cw/cw1.py
@@ -0,0 +1,5 @@
+num = 12
+if num<18:
+    print("underage")
+else:
+    print("adult")
---
+++ Day 19/cw/cw2.py
@@ -0,0 +1,9 @@
+num = int(input())
+if num>0:
+    print("positive")
+
+if num == 0:
+    print("zero")
+
+if num<0:
+    print("negative")
