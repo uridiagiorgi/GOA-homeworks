a19138732d2e213a42c219b7dbbfb6024015d780
uridiagiorgi <qweqwe@gmail.com>
2024-10-18 17:36:16 +0400

    second commit

---
+++ Day 35/cw/cw1.py
@@ -0,0 +1,7 @@
+def tri(ferdi1, ferdi2, fudze):
+    perimeter = ferdi1 + ferdi2 + fudze
+    area = ferdi1*ferdi2*fudze
+    return perimeter, area
+result1, result2 = tri(10, 20, 30)
+print(result1)
+print(result2)
