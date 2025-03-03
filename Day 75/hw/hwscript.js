3c01750b51642f16c777b2ce350bba606369c46b
uridiagiorgi <qweqwe@gmail.com>
2025-03-03 18:57:00 +0400

    second commit

---
+++ Day 75/hw/hwscript.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    console.log("4 + 5 =", 4 + 5);
+    console.log("11 - 7 =", 11 - 7);
+    console.log("7 * 6 =", 7 * 6);
+    console.log("10 / 2 =", 10 / 2);
+};
+
+function styleButton() {
+    let button = document.getElementById("button");
+    button.style.color = "purple";
+    button.style.backgroundColor = "pink";
+    button.style.borderRadius = "8px";
+    button.style.width = "140px";
+}
